74a385054b2fcc80ad9fd3e9fe63c792df6255ba
LuxorGonsalez <luxor.gonsalez@gmail.com>
2022-07-13 01:05:34 +0300

    Added .gitignore

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int bonusPrice = 20;
+
+        int bonus = amount / bonusPrice;
+
+        System.out.println("Количество бонусных миль: " + bonus);
+
+
+    }
+}
